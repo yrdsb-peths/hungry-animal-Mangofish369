442fe142acee5902329efeff293faddddce8f1ac
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2022-11-30 14:47:04 -0500

    Elephant movement & Falling pears
    Elephant class moves left and right 
    Pears fall at random location
    Added eat() method

--- Elephant.java
+++ Elephant.java
@@ -4,11 +4,30 @@ public class Elephant extends Actor
 { 
     public void act() 
     {
-        // Add your action code here.
-        move(4);
+       if(Greenfoot.isKeyDown("a")){
+            move(-10);
+       }
         
-        if(isAtEdge()){
-            turn(180);
-        }
+       if(Greenfoot.isKeyDown("d")){
+            move(10);
+       }
+       
+       consume();
+       
+       /* 
+        * move(speed);
+        * if(Greenfoot.isKeyDown("key")){ }
+        * int x = Greenfoot.getRandomNumber(limit);  
+        * if(isAtEdge()){ }
+        */
     }    
+    
+    // Eat pear when in contact and spawn a new one
+    public void consume(){
+        if(isTouching(Pear.class)){
+            removeTouching(Pear.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.spawnPear();
+        }
+    }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -6,9 +6,20 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        // Make our Elephant
         Elephant elephant = new Elephant();
         addObject(elephant, 0,200);
-        Pear pear = new Pear ();
-        addObject(pear, 300,0);
+        
+        // Make Pear
+        spawnPear();
+    } 
+    
+    // Spawn pear at random x location
+        public void spawnPear(){
+            Pear pear = new Pear();
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = 10;
+            addObject(pear,x, y);
     }
 }
