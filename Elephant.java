cc3dc2f6f105b3b428d7411c2984d9dececab5ba
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2022-12-07 14:16:53 -0500

    Elephant animate

--- Elephant.java
+++ Elephant.java
@@ -1,7 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Actor
-{ 
+{
+    
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public Elephant(){
+        for(int i = 0; i< idle.length; i++){
+            idle[i] = new GreenfootImage("images/elephant_idle/idle"+ i +".png");
+        }
+        setImage(idle[0]);
+    }    
     public void act() 
     {
        if(Greenfoot.isKeyDown("a")){
@@ -15,12 +25,7 @@ public class Elephant extends Actor
        
        consume();
        
-       /* 
-        * move(speed);
-        * if(Greenfoot.isKeyDown("key")){ }
-        * int x = Greenfoot.getRandomNumber(limit);  
-        * if(isAtEdge()){ }
-        */
+       animateElephant();
     }    
     
     // Eat pear when in contact and spawn a new one
@@ -34,6 +39,9 @@ public class Elephant extends Actor
         }
     }
     
-    //Sound object
-    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    int imageIndex = 0;
+    public void animateElephant(){
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex+1) % idle.length;
+    }    
 }
